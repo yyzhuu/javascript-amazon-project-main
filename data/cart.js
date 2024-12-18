5c4c9e9603a577fcca7ea328ac1a9a4b4f9e16bf
SuperSimpleDev <simon@supersimple.dev>
2024-12-18 21:13:24 +0800

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -5,9 +5,11 @@ if(!cart){  //default
   cart = [{ 
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
     quantity:2, 
+    deliveryOptionsId: '1', 
   }, { 
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d', 
     quantity:1, 
+    deliveryOptionsId: '2', 
   }
   ]; 
 }
@@ -31,6 +33,7 @@ export function addToCart(productId) {
     cart.push({ 
       productId, 
       quantity:1, 
+      deliveryOptionsId: '1', //new product, default is 1 
     }); 
   }
 
