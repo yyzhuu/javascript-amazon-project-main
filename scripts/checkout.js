0866a541c02c0df43b1ce7a7b43774789eb923f1
SuperSimpleDev <simon@supersimple.dev>
2024-12-18 22:16:13 +0800

    Regenerate HTML when updating delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -9,9 +9,9 @@ import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 const today = dayjs(); 
 const deliveryDate = today.add(7, 'days');
 
-
-let cartSummaryHTML = ''; 
-cart.forEach((cartItem) => { 
+function renderOrderSummary() { 
+  let cartSummaryHTML = ''; 
+  cart.forEach((cartItem) => { 
 
   const productId = cartItem.productId; 
   let matchingProduct; 
@@ -146,6 +146,9 @@ document.querySelectorAll('.js-delivery-option')
     element.addEventListener('click', () => { 
       const {productId, deliveryOptionId} = element.dataset; 
       updateDeliveryOption(productId, deliveryOptionId); 
-
+      renderOrderSummary(); 
     }); 
   })
+}
+
+renderOrderSummary(); 
