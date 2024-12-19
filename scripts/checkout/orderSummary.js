795acfe35f41f00a89466253105c4156d3030c08
SuperSimpleDev <simon@supersimple.dev>
2024-12-19 12:16:03 +0800

    Create automated tests for formatCurrency

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -7,8 +7,6 @@ import { renderPaymentSummary } from './paymentSummary.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
 
-
-
 export function renderOrderSummary() { 
   let cartSummaryHTML = ''; 
   cart.forEach((cartItem) => { 
