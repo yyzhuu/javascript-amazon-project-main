3f3a45f61a28bb0fdf1b4d2370d8b2620c316f7c
SuperSimpleDev <simon@supersimple.dev>
2024-12-19 11:13:03 +0800

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,7 +1,8 @@
 import { cart, removeFromCart, updateDeliveryOption } from '../../data/cart.js';
-import { deliveryOptions } from '../../data/deliveryOptions.js';
-import { products } from '../../data/products.js';
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
+
 
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
@@ -15,22 +16,10 @@ export function renderOrderSummary() {
   cart.forEach((cartItem) => { 
 
   const productId = cartItem.productId; 
-  let matchingProduct; 
+  const matchingProduct = getProduct(productId); 
   
-  products.forEach((product) => { 
-    if(product.id === productId) { 
-      matchingProduct = product; 
-    }
-  }); 
-
   const deliveryOptionId = cartItem.deliveryOptionId;
-  
-  let deliveryOption='';  
-  deliveryOptions.forEach((option) => { 
-    if(option.id === deliveryOptionId){ 
-      deliveryOption = option; 
-    }
-  }); 
+  const deliveryOption = getDeliveryOption(deliveryOptionId); 
 
 
 
