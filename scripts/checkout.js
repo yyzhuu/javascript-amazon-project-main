3079dd960749014d9d79543d3cda3ff268a95ad1
SuperSimpleDev <simon@supersimple.dev>
2024-12-21 16:08:57 +0800

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
